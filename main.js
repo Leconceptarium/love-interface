6efdb01c6cfce43cf79db98ed781957deba7ab39
Leconceptarium <toitotmax@gmail.com>
2025-06-09 15:54:36 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+const jeux = ["jeu1", "jeu2", "jeu3", "jeu4", "jeu5"];
+let jeuxRestants = [...jeux];
+
+function spinWheel() {
+  if (jeuxRestants.length === 0) jeuxRestants = [...jeux];
+  const index = Math.floor(Math.random() * jeuxRestants.length);
+  const selection = jeuxRestants.splice(index, 1)[0];
+  window.location.href = "jeux/" + selection + ".html";
+}
+
+function updateLifeBars() {
+  ['life1', 'life2'].forEach(id => {
+    const val = localStorage.getItem(id) || 100;
+    const el = document.getElementById(id);
+    if (el) el.style.width = val + "%";
+  });
+}
+
+window.onload = updateLifeBars;
